a5d4ca2c4bb85d6f44a6dec69fe6270b75aec866
Michael Van Sickle <mvansickle1@gmail.com>
2020-03-21 07:33:40 -0400

    add tests for module 3

--- module3_test.go
+++ module3_test.go
@@ -1,9 +1,14 @@
 package alog
 
-import "testing"
+import (
+	"bytes"
+	"strings"
+	"testing"
+	"time"
+)
 
-// 01-01
-func TestNewInitializesShutdownChannels(t *testing.T) {
+// 011
+func TestNewInitializesShutdownChannelsModule3(t *testing.T) {
 	alog := New(nil)
 	if alog.shutdownCh == nil {
 		t.Error("shutdownCh field not initialized")
@@ -14,4 +19,91 @@ func TestNewInitializesShutdownChannels(t *testing.T) {
 	}
 }
 
-// 02-01
+// 02
+
+func TestShutdownMethodModule3(t *testing.T) {
+	alog := New(nil)
+	alog.shutdownCompleteCh = make(chan struct{}, 1)
+	alog.shutdown()
+	time.Sleep(100 * time.Millisecond)
+	select {
+	case _, ok := <-alog.msgCh:
+		if ok {
+			t.Error("msgCh not closed by shutdown() method")
+		}
+	default:
+		t.Error("msgCh not closed by shutdown() method")
+	}
+	select {
+	case <-alog.shutdownCompleteCh:
+	default:
+		t.Error("shutdown() doesn't send message to shutdownCompleteCh")
+	}
+
+}
+
+// 03
+
+func TestStartMethodCallsShutdownModule3(t *testing.T) {
+	b := bytes.NewBuffer([]byte{})
+	alog := New(b)
+	alog.shutdownCh = make(chan struct{}, 1)
+	alog.shutdownCompleteCh = make(chan struct{}, 1)
+	go alog.Start()
+	alog.shutdownCh <- struct{}{}
+	time.Sleep(100 * time.Millisecond)
+
+	select {
+	case _, ok := <-alog.msgCh:
+		if ok {
+			t.Error("Passing message to shutdownCh doesn't call shutdown()")
+		}
+	default:
+		t.Error("Passing message to shutdownCh doesn't call shutdown()")
+	}
+	select {
+	case <-alog.shutdownCompleteCh:
+	default:
+		t.Error("Passing message to shutdownCh doesn't call shutdown()")
+	}
+	if b.Len() != 0 {
+		t.Error("Passing message to shutdownCh doesn't break out of the Start method's for loop. " +
+			"Note that 'break' statements can be used for select and for loops so a label might be " +
+			"required to break out the loop.")
+	}
+}
+
+// 04
+
+func TestStopMethodModule3(t *testing.T) {
+	alog := New(nil)
+	alog.shutdownCh = make(chan struct{}, 1)
+	alog.shutdownCompleteCh = make(chan struct{}, 1)
+	alog.shutdownCompleteCh <- struct{}{}
+	alog.Stop()
+	select {
+	case <-alog.shutdownCh:
+	default:
+		t.Error("Stop() method doesn't send signal to shutdownCh channel")
+	}
+	select {
+	case <-alog.shutdownCompleteCh:
+		t.Error("Stop() method doesn't wait for signal from shutdownCompleteCh channel")
+	default:
+	}
+}
+
+// 05
+
+func TestWriteAllBeforeShutdownModule3(t *testing.T) {
+	b := bytes.NewBuffer([]byte{})
+	alog := New(sleepingWriter{b})
+	go alog.Start()
+	alog.msgCh <- "first"
+	alog.msgCh <- "second"
+	alog.Stop()
+	written := b.String()
+	if !strings.Contains(written, "first") || !strings.Contains(written, "second") {
+		t.Error("Not all messages written before logger shutdown")
+	}
+}
